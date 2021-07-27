8db19a4ab232b9dadfb22833aa80f66d13cfbab2
Kevin Loughead <kvnloughead@gmail.com>
2021-07-26 20:57:45 -0400

    refactor: move shelf specific code to shelf.py

--- parse.py
+++ parse.py
@@ -4,7 +4,7 @@ Parses incoming command line arguments.
 
 from typing import cast
 from utils.constants import OPTIONS, OPTION_DEFAULTS
-from shelf import shelf
+from shelf import update_category_shelf
 
 def parse_edit_args(args):
     """
@@ -22,16 +22,6 @@ def parse_edit_args(args):
     options = [OPTIONS[arg.replace('-', '')] for arg in args[::2]]
     kwargs = dict(zip(options, args[1::2]))
     kwargs = { **OPTION_DEFAULTS, **kwargs }    # update kwargs with defaults
-
-    # initialize shelf['category'] if necessary
-    if 'category' not in shelf.keys():
-        shelf['category'] = [] 
-    # attempt to prevent proliferation of category pairs differing only in plurality
-    if not kwargs['category'].endswith('s') and f"{kwargs['category']}s" in shelf['category']:
-        ans = input(f"Did you mean \"{kwargs['category']}s\"? Yes or No\n")
-        if ans.lower().startswith('y'):
-            kwargs['category'] = f"{kwargs['category']}s"
-    # add category to shelf
-    shelf['category'] = list(set(shelf['category'] + [kwargs['category']]))
-
+    update_category_shelf(kwargs)
     return kwargs
+
