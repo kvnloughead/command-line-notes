e25ade85827298c03a3924ac93d575dd55a62861
Kevin Loughead <kvnloughead@gmail.com>
2021-11-28 10:59:14 -0500

    fix git push issue

--- push.py
+++ push.py
@@ -1,10 +1,10 @@
 """
-Runs `git add -A`, `git commit` and `git push` in your .notes directory 
-(or .notes-dev if in -dev mode). 
+Runs `git add -A`, `git commit` and `git push` in your .notes directory
+(or .notes-dev if in -dev mode).
 
 Assumes a certain amount of setup, like running `git init` and syncing to gh. TODO - elaborate
 
-Always pushes to wherever you set the upstream repo. No support for anything fancy, like branches. But 
+Always pushes to wherever you set the upstream repo. No support for anything fancy, like branches. But
 you can always run `cln -o` to open the directory in your editor and git to your heart's content.
 """
 
@@ -14,9 +14,9 @@ from datetime import datetime
 from utils.constants import get_base_path
 from commit import commit
 
+
 def push(args):
-  print('here')
-  BASE_PATH = get_base_path(args)
-  commit(args)
-  remote_repo = 'dev' if args.dev else ''
-  os.system(f'cd {BASE_PATH} && git push origin {remote_repo}')
+    BASE_PATH = get_base_path(args)
+    commit(args)
+    remote_repo = 'dev' if args.dev else ''
+    os.system(f'cd {BASE_PATH} && git push origin {remote_repo}')
