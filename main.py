e6e97ab47fb047057780fad8818aa17c60a02e29
Kevin Loughead <kvnloughead@gmail.com>
2022-01-02 15:02:51 -0500

    reorganize flags

--- main.py
+++ main.py
@@ -21,7 +21,33 @@ from grep import grep
 from find import find
 from rename import rename
 
+# main parser
 parser = argparse.ArgumentParser()
+
+# top level args
+parser.add_argument(
+    "-dev", help="Developer mode uses different directory for notes.", action="store_true", default=DEV)
+
+# parent parser with `e|editor` flag
+editor_parent_parser = argparse.ArgumentParser(add_help=False)
+editor_parent_parser.add_argument(
+    "-e", "--editor", help="specifies editor; defaults to nano", default=EDITOR)
+
+# parent parser for metadata related options
+metadata_parent_parser = argparse.ArgumentParser(add_help=False)
+metadata_parent_parser.add_argument("-c", "--category", help="Specifies category of the note. Notes are organized into directories based on their category. Defaults to 'defaultmetadata_parent_'",
+                                    default="default")
+metadata_parent_parser.add_argument("-t", "--tags", help="A comma separated list of tags.",
+                                    default="")
+metadata_parent_parser.add_argument("-a", "--author", help="The author of the note.",
+                                    default=AUTHOR)
+# parser.add_argument("-c", "--category", help="Specifies category of the note. Notes are organized into directories based on their category. Defaults to 'default'",
+#                     default="default")
+# parser.add_argument("-t", "--tags", help="A comma separated list of tags. Currently not used.",
+#                     default="")
+# parser.add_argument("-a", "--author", help="The author of the note.",
+#                     default=AUTHOR)
+
 
 subparsers = parser.add_subparsers(
     title="subcommands", description="", help="-"*10)
@@ -29,6 +55,7 @@ subparsers = parser.add_subparsers(
 edit_parser = subparsers.add_parser(
     "edit",
     aliases=["e"],
+    parents=[editor_parent_parser, metadata_parent_parser],
     help="`cln edit foo` opens a note called `foo` for editing, creating it if it doesn't already exist")
 edit_parser.add_argument(
     "name", nargs="?", help="the name of the note to be created/edited")
@@ -37,6 +64,7 @@ edit_parser.set_defaults(func=edit)
 delete_parser = subparsers.add_parser(
     "delete",
     aliases=["d"],
+    parents=[metadata_parent_parser],
     help="`cln delete foo` deletes a note called `foo`, if it exists. Prompts for confirmation.")
 delete_parser.add_argument(
     "name",
@@ -47,7 +75,8 @@ delete_parser.set_defaults(func=delete)
 show_parser = subparsers.add_parser(
     "show",
     aliases=["s"],
-    help="`cln show` prints a list of all notes to terminal")
+    help="`cln show` prints a list of all notes to terminal",
+    parents=[metadata_parent_parser])
 show_parser.add_argument(
     "--pager", "-p", action="store_true", help="Pipes output of `cln show` to `less`.")
 show_parser.set_defaults(func=show)
@@ -55,6 +84,7 @@ show_parser.set_defaults(func=show)
 open_parser = subparsers.add_parser(
     "opendir",
     aliases=["o"],
+    parents=[editor_parent_parser],
     help="""`cln opendir` opens the notes directory in the editor.\n
         Currently this will only work if your default editor (found in 
         constants.py) accepts a directory as an argument.""")
@@ -63,26 +93,27 @@ open_parser.set_defaults(func=opendir)
 push_parser = subparsers.add_parser(
     "push",
     aliases=["p"],
-    help="runs `git add -A`, `git commit` and `git push` in your notes directory")
+    help="Runs `git add -A`, `git commit` and `git push` in your notes directory.\nRunning `cln` with the `-dev` flag changes the directory and repo.")
 push_parser.set_defaults(func=push)
 
 commit_parser = subparsers.add_parser(
     "commit",
     aliases=["c"],
-    help="runs `git add -A` and `git commit` in your notes directory")
+    help="Runs `git add -A` and `git commit` in your notes directory. \nRunning `cln` with the `-dev` flag changes the directory and repo.")
 commit_parser.set_defaults(func=commit)
 
 grep_parser = subparsers.add_parser(
     "grep",
     aliases=["g"],
-    help="`cln grep foo` greps for `foo` in your notes directory")
+    parents=[metadata_parent_parser],
+    help="`cln grep foo` greps for `foo` in your notes directory.")
 grep_parser.add_argument("pattern", help="the pattern to be grepped for")
 grep_parser.set_defaults(func=grep)
 
 find_parser = subparsers.add_parser(
     "find",
     aliases=["f"],
-    help="finds all files with similar names")
+    help="`cln find foo` finds all files with the pattern `foo` in their filename.")
 find_parser.add_argument(
     "pattern", help="the pattern to search for in the note names")
 find_parser.set_defaults(func=find)
@@ -97,17 +128,6 @@ rename_parser.add_argument(
     "newname", help="the new name for the note")
 rename_parser.set_defaults(func=rename)
 
-
-parser.add_argument("-c", "--category", help="Specifies category of the note. Notes are organized into directories based on their category. Defaults to 'default'",
-                    default="default")
-parser.add_argument("-t", "--tags", help="A comma separated list of tags. Currently not used.",
-                    default="")
-parser.add_argument("-a", "--author", help="The author of the note.",
-                    default=AUTHOR)
-parser.add_argument(
-    "-e", "--editor", help="specifies editor; defaults to code", default=EDITOR)
-parser.add_argument(
-    "-dev", help="Developer mode uses different directory for notes.", action="store_true", default=DEV)
 
 args = parser.parse_args()
 
